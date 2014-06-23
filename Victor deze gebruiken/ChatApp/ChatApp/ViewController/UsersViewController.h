7362386173897837b21f1dc660816c3654677bf7
Thijs <Thijs_velden@msn.com>
2014-06-23 22:41:33 +0200

    Beta

---
+++ Victor deze gebruiken/ChatApp/ChatApp/ViewController/UsersViewController.h
@@ -0,0 +1,13 @@
+//
+//  СhatViewController.h
+//  ChatApp
+//
+//  Created by Thijs van der Velden on 10/06/2014
+//  Copyright (c) 2014 Thijs van der Velden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UsersViewController : UIViewController
+
+@end
